ca00d3a9f6bca195fddc478127d32793ebc86959
thundervikes10166 <thundervikes10166@gmail.com>
2026-01-16 02:43:47 -0800

    update sample files
    Add sample codes to assist in creating components and applying them.

---
+++ RobotMain/components/sample_drive_motor.py
@@ -0,0 +1,14 @@
+from magicbot import will_reset_to
+import rev
+
+class SampleDriveMotor:
+    power = will_reset_to(0)
+
+    def inject(self, motor: rev.SparkMax):
+        self.motor = motor
+
+    def set_power(self, value: float):
+        self.power = value
+
+    def execute(self):
+        self.motor.set(self.power)

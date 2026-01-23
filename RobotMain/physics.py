1c318f205489ca758a3409b2f8d18e38b82f2002
jciricuti <joseciri0211@gmail.com>
2026-01-22 19:52:56 -0800

    Update physics.py
    i started the physics pi simulation code c

--- RobotMain/physics.py
+++ RobotMain/physics.py
@@ -1,1 +1,14 @@
 #TODO: Add code here for simulating
+import math
+from wpimath.geometry import Pose2d, Rotation2d
+from wpimath.kinematics import ChassisSpeeds
+
+from wpilib import Field2d, SmartDashboard
+
+def wrap_to_pi(rad: float) -> float:
+    while rad > math.pi:
+        rad -= 2.0 * math.pi
+    while rad < -math.pi:
+        rad += 2.0 * math.pi
+    return rad
+

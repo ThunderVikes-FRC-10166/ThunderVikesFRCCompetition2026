b63ee46993a1b5780d200e9168e67bc8bc99cd87
jnieto2 <joshua.nieto0911@gmail.com>
2026-01-24 14:51:08 -0800

    Update robot.py

--- RobotMain/robot.py
+++ RobotMain/robot.py
@@ -2,6 +2,7 @@ import magicbot
 import wpilib
 import math
 
+from RobotMain.samples.samplecomponentusage import MyRobot
 from components.swerve_drive_sim import SwerveDriveSim
 # ----------------------
 # --------------------------------------------------------
@@ -22,9 +23,14 @@ from components.swerve_drive_sim import SwerveDriveSim
 #
 # Students will fill these methods with real robot code as the robot develops.
 # ------------------------------------------------------------------------------
+def deadband(x: float, db: float = 0.08) -> float:
+    return 0.0 if abs(x) < db else x
+
+
 
 
 class MyRobot(magicbot.MagicRobot):
+    swerveDriveSim = SwerveDriveSim
     """
     This class represents your entire robot program.
 
@@ -41,6 +47,7 @@ class MyRobot(magicbot.MagicRobot):
     # createObjects()
     # --------------------------------------------------------------------------
     def createObjects(self):
+        self.drive = wpilib.XboxController(0)
         """
         This method is called ONCE when the robot boots.
 
@@ -94,6 +101,27 @@ class MyRobot(magicbot.MagicRobot):
     # teleopPeriodic()
     # --------------------------------------------------------------------------
     def teleopPeriodic(self):
+
+        x = -self.driver.getLeftY() #Forward/back
+        y = -self.driver.getLeftX() # strafe
+        rot = -self.driver.getRightx() #rotate
+        x = deadband(x)
+        y = deadband(y)
+        rot = deadband(rot)
+
+        x = -math.copysign(x * x, x)
+        y = -math.copysign(y * y, y)
+        rot = -math.copysign(rot * rot, rot)
+
+        vx = x * self.swerve.max_speed_mps
+        vy = y * self.swerve.max_speed_mps
+        omega = rot * self.swerve.max_omgea_radps
+
+        self.swerve.drive(vx, vy, omega)
+
+    if __name__ == "__main__":
+        wpilib.run(MyRobot)
+        
         """
         This method runs REPEATEDLY during Teleop (about 50 times per second).
 
